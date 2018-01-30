e162e6395f29a16c6a223e755789ffb56737f6b8
Rozmari <32676634+Rozmari@users.noreply.github.com>
2018-01-30 10:21:24 -0500

    Add files via upload

---
+++ sb1y.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+# out is just a variable that is printed into the notification upon script startup
+# you can change it to your own liking.
+
+out=$'Reminders:\n'
+num=1
+
+for ((i=1;i<=$#;i++));
+do
+	if [ ${!i} = "-a" ]
+	then
+		((i++))
+		rem=${!i}
+		echo "$rem" >> remind_data.txt;
+	
+	elif [ ${!i} = "-r"  ];
+	then
+		((i++))
+		n=${!i}
+		let "n -= 1"
+		echo "$n"$'d'
+		sed -e 2d remind_data.txt > r_buf.txt
+		cat r_buf.txt > remind_data.txt
+		rm r_buf.txt;
+	elif [ ${!i} = "-c" ];
+	then
+		cat /dev/null > remind_data.txt;
+	elif [ ${!i} = "-h" ];
+	then
+		echo 'remind.sh - a simple script meant for simple notification-based reminders.'
+		echo 'Usage:' 
+		echo '	-a "[reminder]"	 	adds a new reminder'
+		echo '	-r [number] 		removes reminder by number'
+	        echo '	-c			clears all reminders'
+		echo '';
+	fi
+done;	
+
+while IFS='' read -r line || [[ -n "$line" ]]; do
+	out="$out"$'\n'$"$num. $line "
+	let "num += 1"
+done < "remind_data.txt"
+
+notify-send "$out"
